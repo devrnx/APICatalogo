1affa0f58216c307ad192fcecdf6f3f01cefea8c
Rennan Alves <rennan.alves@blip.ai>
2024-02-09 14:03:48 -0300

    Created Project

---
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,57 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICatalogo.Controllers
+{
+    [ApiController]
+    [Route("v1/[controller]")]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var produtos = _context.Produtos.Select(x => new { x.ProdutoId, x.Nome, x.Preco });
+            return Ok(produtos);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var produto = _context.Produtos.SingleOrDefault(p => p.ProdutoId == id);
+            if (produto is null)
+            {
+                return NotFound();
+            }
+            return Ok(produto);
+        }
+
+        [HttpPost]
+        public IActionResult Post(Produto model)
+        {
+            _context.Produtos.Add(model);
+            return CreatedAtAction(nameof(model), GetById);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update()
+        {
+            var produtos = _context.Produtos;
+            return Ok(produtos);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete()
+        {
+            var produtos = _context.Produtos;
+            return Ok(produtos);
+        }
+    }
+}
---
+++ APICatalogo/Migrations/20240204201923_AlteracaoCampo.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlteracaoCampo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Produtos",
+                newName: "Nome");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Nome",
+                table: "Produtos",
+                newName: "Name");
+        }
+    }
+}
