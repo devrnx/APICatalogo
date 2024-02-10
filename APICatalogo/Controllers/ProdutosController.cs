481f153d9d5d6ea127fafe154c47af3da5fa692e
Rennan Alves <rennan.alves@blip.ai>
2024-02-09 21:38:50 -0300

    Implementation delete

--- APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -67,5 +67,20 @@ namespace APICatalogo.Controllers
             _context.SaveChanges();
             return Ok(produto);
         }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(x => x.ProdutoId == id);
+
+            if (produto is null)
+            {
+                return NotFound("Produto não encontrado.");
+            }
+
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+            return Ok(produto);
+        }
     }
 }
