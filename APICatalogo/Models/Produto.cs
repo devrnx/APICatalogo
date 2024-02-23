9f930d686131f75cf95acc7e15ac4878bdee8b52
Rennan Alves <rennan.alves@blip.ai>
2024-02-23 17:35:00 -0300

    Creating middleware exception

--- APICatalogo/Models/Produto.cs
+++ APICatalogo/Models/Produto.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using APICatalogo.Validations;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 
@@ -9,20 +10,26 @@ namespace APICatalogo.Models
     {
         [Key]
         public int ProdutoId { get; set; }
-        [Required]
-        [StringLength(80)]
+
+        [PrimeiraLetraMaiusculaAtributte]
         public string? Nome { get; set; }
+
         [Required]
         [StringLength(300)]
         public string? Descricao { get; set; }
+
         [Required]
         [Column(TypeName = "decimal(10,2)")]
         public decimal Preco { get; set; }
+
         [Required]
         [StringLength(300)]
         public string? ImagemUrl { get; set; }
+
         public float Estoque { get; set; }
+
         public DateTime DataCadastro { get; set; }
+
         public int CategoriaId { get; set; }
 
         [JsonIgnore]
--- APICatalogo/Program.cs
+++ APICatalogo/Program.cs
@@ -1,4 +1,5 @@
 using APICatalogo.Context;
+using APICatalogo.Extensions;
 using APICatalogo.Services;
 using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
@@ -22,7 +23,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
 });
 
-builder.Services.AddTransient<IMeuServico>();
+builder.Services.AddTransient<IMeuServico, MeuServico>();
 
 var app = builder.Build();
 
@@ -31,6 +32,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.ConfigureExceptionHandler();
 }
 
 app.UseHttpsRedirection();
